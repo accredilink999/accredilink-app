4417ef9019040936c41a42755891ae8945fe3728
Michael Bohanna <mikebohanna.work@gmail.com>
2026-03-02 03:20:40 +0000

    Remove unused generateInviteCode from onboarding page

--- carecallai-website/app/onboarding/page.jsx
+++ carecallai-website/app/onboarding/page.jsx
@@ -367,12 +367,3 @@ export default function OnboardingPage() {
     </section>
   );
 }
-
-function generateInviteCode() {
-  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
-  let code = '';
-  for (let i = 0; i < 8; i++) {
-    code += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return code;
-}
